d3994275646266a3b5d5b69774b1b8148d722473
Fernando <fernandocassiodev@gmail.com>
2024-10-28 19:11:44 -0300

    feat: adiciona as funcionalidades da user-nav

--- components/common/user-nav.tsx
+++ components/common/user-nav.tsx
@@ -56,6 +56,7 @@ export function UserNav() {
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" className="relative h-8 w-8 rounded-full">
               <Avatar className="h-8 w-8">
+                //Colocar avatar padrão 
                 <AvatarImage src="/avatars/01.png" alt="Profile Picture" />
                 <AvatarFallback>SC</AvatarFallback>
               </Avatar>
@@ -64,9 +65,11 @@ export function UserNav() {
           <DropdownMenuContent className="w-56" align="end" forceMount>
             <DropdownMenuLabel className="font-normal">
               <div className="flex flex-col space-y-1">
-                <p className="text-sm font-medium leading-none">shadcn</p>
+                <p className="text-sm font-medium leading-none">
+                  {user.email?.split("@")[0]}
+                </p>
                 <p className="text-xs leading-none text-muted-foreground">
-                  m@example.com
+                  {user.email}
                 </p>
               </div>
             </DropdownMenuLabel>
@@ -76,18 +79,9 @@ export function UserNav() {
                 Profile
                 <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                Billing
-                <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                Settings
-                <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>New Team</DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={handleSignOut}>
               Log out
               <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
             </DropdownMenuItem>
