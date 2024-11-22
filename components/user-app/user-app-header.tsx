356911ec3345cda89d9301f497686ba66e1908fa
Fernando <fernandocassiodev@gmail.com>
2024-11-21 21:30:47 -0300

    feat: user-app-header implemented

--- components/user-app/user-app-header.tsx
+++ components/user-app/user-app-header.tsx
@@ -4,7 +4,16 @@ import { UserNav } from "../common/user-nav";
 import React from "react";
 
 function UserAppHeader() {
-  return <div>user-app-header</div>;
+  return (
+    <header>
+      <nav className="flex justify-between items-center m-4">
+        <span className="font-extrabold">
+          re<span className="font-extralight">Store</span>
+        </span>
+        <UserNav />
+      </nav>
+    </header>
+  );
 }
 
 export default UserAppHeader;
