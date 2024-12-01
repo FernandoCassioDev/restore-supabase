d378cd95fce7caeb999ccf86778536bea49fa628
Fernando <fernandocassiodev@gmail.com>
2024-11-30 21:41:40 -0300

    feat: component default names changed

--- app/user-app/page.tsx
+++ app/user-app/page.tsx
@@ -5,12 +5,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect, RedirectType } from "next/navigation";
 
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { Separator } from "@/components/ui/separator";
 import { PlusCircleIcon } from "lucide-react";
@@ -33,51 +28,51 @@ export default async function UserApp() {
 
   return (
     <>
-    <div className=" md:block">
-      {/* <Menu /> */}
-      <UserAppHeader />
-      <div className="border-t">
-        <div className="bg-background">
-          <div className="grid lg:grid-cols-5">
-            {/* <Sidebar playlists={playlists} className="hidden lg:block" /> */}
-            <div className="col-span-3 lg:col-span-4 lg:border-l">
-              <div className="h-full px-4 py-6 lg:px-8">
-                <Tabs defaultValue="music" className="h-full space-y-6">
-                  <div className="space-between flex items-center">
-                    <TabsList>
-                      <TabsTrigger value="music" className="relative">
-                        Music
-                      </TabsTrigger>
-                      <TabsTrigger value="podcasts">Podcasts</TabsTrigger>
-                      <TabsTrigger value="live" disabled>
-                        Live
-                      </TabsTrigger>
-                    </TabsList>
-                    <div className="ml-auto mr-4">
-                      <Button>
-                        <PlusCircleIcon />
-                        Add music
-                      </Button>
-                    </div>
-                  </div>
-                  <TabsContent
-                    value="music"
-                    className="border-none p-0 outline-none"
-                  >
-                    <div className="flex items-center justify-between">
-                      <div className="space-y-1">
-                        <h2 className="text-2xl font-semibold tracking-tight">
-                          Listen Now
-                        </h2>
-                        <p className="text-sm text-muted-foreground">
-                          Top picks for you. Updated daily.
-                        </p>
+      <div className=" md:block">
+        {/* <Menu /> */}
+        <UserAppHeader />
+        <div className="border-t">
+          <div className="bg-background">
+            <div className="grid lg:grid-cols-5">
+              {/* <Sidebar playlists={playlists} className="hidden lg:block" /> */}
+              <div className="col-span-3 lg:col-span-4 lg:border-l">
+                <div className="h-full px-4 py-6 lg:px-8">
+                  <Tabs defaultValue="music" className="h-full space-y-6">
+                    <div className="space-between flex items-center">
+                      <TabsList>
+                        <TabsTrigger value="photos" className="relative">
+                          Photos
+                        </TabsTrigger>
+                        <TabsTrigger value="documents">Documents</TabsTrigger>
+                        <TabsTrigger value="other" disabled>
+                          Other
+                        </TabsTrigger>
+                      </TabsList>
+                      <div className="ml-auto mr-4">
+                        <Button>
+                          <PlusCircleIcon />
+                          Add Collection
+                        </Button>
                       </div>
                     </div>
-                    <Separator className="my-4" />
-                    <div className="relative">
-                      {/* Lista */}
-                      {/* <ScrollArea>
+                    <TabsContent
+                      value="photos"
+                      className="border-none p-0 outline-none"
+                    >
+                      <div className="flex items-center justify-between">
+                        <div className="space-y-1">
+                          <h2 className="text-2xl font-semibold tracking-tight">
+                            Listen Now
+                          </h2>
+                          <p className="text-sm text-muted-foreground">
+                            Top picks for you. Updated daily.
+                          </p>
+                        </div>
+                      </div>
+                      <Separator className="my-4" />
+                      <div className="relative">
+                        {/* Lista */}
+                        {/* <ScrollArea>
                         <div className="flex space-x-4 pb-4">
                           {listenNowAlbums.map((album) => (
                             <AlbumArtwork
@@ -92,19 +87,19 @@ export default async function UserApp() {
                         </div>
                         <ScrollBar orientation="horizontal" />
                       </ScrollArea> */}
-                    </div>
-                    <div className="mt-6 space-y-1">
-                      <h2 className="text-2xl font-semibold tracking-tight">
-                        Made for You
-                      </h2>
-                      <p className="text-sm text-muted-foreground">
-                        Your personal playlists. Updated daily.
-                      </p>
-                    </div>
-                    <Separator className="my-4" />
-                    <div className="relative">
-                      {/* Lista 2 */}
-                      {/* <ScrollArea>
+                      </div>
+                      <div className="mt-6 space-y-1">
+                        <h2 className="text-2xl font-semibold tracking-tight">
+                          Made for You
+                        </h2>
+                        <p className="text-sm text-muted-foreground">
+                          Your personal playlists. Updated daily.
+                        </p>
+                      </div>
+                      <Separator className="my-4" />
+                      <div className="relative">
+                        {/* Lista 2 */}
+                        {/* <ScrollArea>
                         <div className="flex space-x-4 pb-4">
                           {madeForYouAlbums.map((album) => (
                             <AlbumArtwork
@@ -119,32 +114,32 @@ export default async function UserApp() {
                         </div>
                         <ScrollBar orientation="horizontal" />
                       </ScrollArea> */}
-                    </div>
-                  </TabsContent>
-                  <TabsContent
-                    value="podcasts"
-                    className="h-full flex-col border-none p-0 data-[state=active]:flex"
-                  >
-                    <div className="flex items-center justify-between">
-                      <div className="space-y-1">
-                        <h2 className="text-2xl font-semibold tracking-tight">
-                          New Episodes
-                        </h2>
-                        <p className="text-sm text-muted-foreground">
-                          Your favorite podcasts. Updated daily.
-                        </p>
                       </div>
-                    </div>
-                    <Separator className="my-4" />
-                    {/* <PodcastEmptyPlaceholder /> */}
-                  </TabsContent>
-                </Tabs>
+                    </TabsContent>
+                    <TabsContent
+                      value="documents"
+                      className="h-full flex-col border-none p-0 data-[state=active]:flex"
+                    >
+                      <div className="flex items-center justify-between">
+                        <div className="space-y-1">
+                          <h2 className="text-2xl font-semibold tracking-tight">
+                            New Episodes
+                          </h2>
+                          <p className="text-sm text-muted-foreground">
+                            Your favorite podcasts. Updated daily.
+                          </p>
+                        </div>
+                      </div>
+                      <Separator className="my-4" />
+                      {/* <PodcastEmptyPlaceholder /> */}
+                    </TabsContent>
+                  </Tabs>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
 }
